d6964af12793741d2ae40fe73ab41f2696a98edb
harrykuskus <harrykusumabhakti@gmail.com>
2022-06-13 00:08:50 +0800

    Add sintax

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require("fs");
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,26 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+
+const bacaData = (fncallback) => {
+  const result = [];
+
+  fs.readFile(file1, (err, data) => {
+    if (err) return console.log(`Error : ${err}`);
+    result.push(JSON.parse(data).message.split(" ")[1]);
+
+    fs.readFile(file2, "utf-8", (err, data) => {
+      if (err) return console.log(`Error : ${err}`);
+      result.push(JSON.parse(data)[0].message.split(" ")[1]);
+    });
+
+    fs.readFile(file3, "utf-8", (err, data) => {
+      if (err) return console.log(`Error : ${err}`);
+      result.push(JSON.parse(data)[0].data.message.split(" ")[1]);
+      return fncallback(err, result);
+    });
+  });
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
